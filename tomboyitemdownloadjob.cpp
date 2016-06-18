cd9da32cca07682870102cc80aa274c53509f49f
Stefan Staeglich <stefan@draakgard.de>
2016-06-18 12:31:52 +0200

    KMime::Message::Ptr notes content

--- tomboyitemdownloadjob.cpp
+++ tomboyitemdownloadjob.cpp
@@ -2,13 +2,14 @@
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>>
+#include <KMime/Message>
 
 
 TomboyItemDownloadJob::TomboyItemDownloadJob(const Akonadi::Item &item, QObject *parent)
     : TomboyJobBase(parent)
 {
     resultItem.setMimeType("application/x-vnd.kde.notes");
-    resultItem.setId(item.id());
+    //resultItem.setId(item.id());
     resultItem.setRemoteId(item.remoteId());
     resultItem.setParentCollection(item.parentCollection());
 }
@@ -29,14 +30,25 @@ void TomboyItemDownloadJob::start()
     QJsonDocument document = QJsonDocument::fromJson(reply->readAll(), Q_NULLPTR);
 
     QJsonObject jo = document.object();
-    QJsonObject note = jo["note"].toArray().first().toObject();
+    QJsonObject jsonNote = jo["note"].toArray().first().toObject();
 
-    resultItem.setRemoteRevision(note["last-sync-revision"].toString());
+    resultItem.setRemoteRevision(jsonNote["last-sync-revision"].toString());
 
     // Set timestamp
-    QDateTime modifyTime = QDateTime::fromString(note["last-change-date"].toString().mid(0, 19), Qt::ISODate);
-    modifyTime.setOffsetFromUtc(note["last-change-date"].toString().mid(27, 2).toInt() * 60 * 60);
+    QDateTime modifyTime = QDateTime::fromString(jsonNote["last-change-date"].toString().mid(0, 19), Qt::ISODate);
+    modifyTime.setOffsetFromUtc(jsonNote["last-change-date"].toString().mid(27, 2).toInt() * 60 * 60);
     resultItem.setModificationTime(modifyTime);
+
+    KMime::Message::Ptr akonadiNote = KMime::Message::Ptr(new KMime::Message);
+    akonadiNote->subject(true)->fromUnicodeString( jsonNote["title"].toString(), "utf-8" );
+
+    akonadiNote->contentType()->setMimeType("text/plain");
+    akonadiNote->contentType()->setCharset("utf-8");
+    akonadiNote->contentTransferEncoding(true)->setEncoding(KMime::Headers::CEquPr);
+    akonadiNote->mainBodyPart()->fromUnicodeString(jsonNote["note-content"].toString().toUtf8());
+
+    akonadiNote->assemble();
+    resultItem.setPayload<KMime::Message::Ptr>(akonadiNote);
 
     setError(0);
     emitResult();
