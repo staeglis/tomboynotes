cec9006c3655070244d97cda9028c992bd728ffb
Stefan Staeglich <stefan@draakgard.de>
2016-06-23 10:20:22 +0200

    comments

--- tomboynotesresource.h
+++ tomboynotesresource.h
@@ -19,32 +19,34 @@ public Q_SLOTS:
     void configure(WId windowId) Q_DECL_OVERRIDE;
 
 protected Q_SLOTS:
+    // Standard akonadi slots
     void retrieveCollections() Q_DECL_OVERRIDE;
     void retrieveItems(const Akonadi::Collection &col) Q_DECL_OVERRIDE;
     bool retrieveItem(const Akonadi::Item &item, const QSet<QByteArray> &parts) Q_DECL_OVERRIDE;
+
+    // Slots for Job result handling
     void onAuthorizationFinished(KJob *kjob);
     void onCollectionsRetrieved(KJob *kjob);
     void onItemRetrieved(KJob *kjob);
     void onItemsRetrieved(KJob *kjob);
 
 private Q_SLOTS:
+    // Status handling
     void clearStatusMessage();
 
 protected:
     void aboutToQuit() Q_DECL_OVERRIDE;
 
+    // Standard akonadi
     void itemAdded(const Akonadi::Item &item, const Akonadi::Collection &collection) Q_DECL_OVERRIDE;
     void itemChanged(const Akonadi::Item &item, const QSet<QByteArray> &parts) Q_DECL_OVERRIDE;
     void itemRemoved(const Akonadi::Item &item) Q_DECL_OVERRIDE;
 
 private:
     bool configurationValid();
+
+    // Status handling
     void showError(const QString errorText);
-
-    QNetworkAccessManager *manager;
-    O1Requestor *requestor;
-    O1Tomboy *o1;
-
     QTimer *mStatusMessageTimer;
 };
 
