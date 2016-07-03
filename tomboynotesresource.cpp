9ac71bc3d85235cb96e41a3680a0794931bb21d0
Stefan Staeglich <stefan@draakgard.de>
2016-07-03 23:23:55 +0200

    e-mail

--- tomboynotesresource.cpp
+++ tomboynotesresource.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2016 Stefan Stäglich
+    Copyright (c) 2016 Stefan Stäglich <sstaeglich@kdemail.net>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
